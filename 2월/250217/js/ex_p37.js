5d79e1678ff26df1db81013ccb45dcbcbbf7150b
jjh6965 <jaehun6965@gmail.com>
2025-02-17 12:02:12 +0900

    0214-과제

---
+++ 2월/250217/js/ex_p37.js
@@ -0,0 +1,72 @@
+// document.write("환영합니다.","<br>");
+
+// var box;
+// box=100;
+// box=30;
+// document.write(box,"<br><br>");
+
+//  var s= "javascript";
+//  var num = "100";
+//  var tag = "<h1>Sting<h1>";
+
+//  var s = 100;
+//  var t = Number("500");
+
+//  var s = true;
+//  var t = 10 >= 100;
+//  var k = Boolean("hello");
+
+//  var m = Boolean(0);
+//  var k = Boolean("홍길동");
+
+//  var a = true;
+//  var b = false;
+//  var c = 10 > 5;
+//  var d = Boolean(null);
+
+
+//  // let z = 30;
+ 
+//  document.write(a,"<br>");
+//  document.write(b,"<br>");
+//  document.write(c,"<br>");
+//  document.write(d,"<br>");
+
+//  var s;
+//  var t="hello";
+//  t=null;
+
+//  var num = 100;
+//  var str = "자바스크립트";
+ 
+//  document.write(typeof num, "<br>");
+//  document.write(typeof str);
+
+//  var num = 10;
+//  var num = 20;
+
+//  let num = 10;
+//  let num = 20;
+
+//  let num = 10;
+//  num = 20;
+
+//  const PI = 3.14;
+//  PI = 20;
+
+//  var num_1 = 100;
+//  var num_1 = 200;
+//  document.write(num_1,"<br>");
+ 
+//  let num_2 = 10;
+//  num_2 = 20;
+//  document.write(num_2,"<br>");
+
+//  const num_3 = 50;
+//  document.write(num_3,"<br>");
+
+// var 1num = 10;
+var $num = 10;
+
+// var @num = 10;
+var num100 = 10;
