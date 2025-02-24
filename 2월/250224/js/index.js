28693faa1c53869fb0dc8c8c9c12d8177c21657f
jjh6965 <jaehun6965@gmail.com>
2025-02-24 17:49:38 +0900

    0224 배열

---
+++ 2월/250224/js/index.js
@@ -0,0 +1,14 @@
+// var s2 = document.querySelector("#s2 > .result");
+
+// document.getElementById("s1").innerHTML = "test33";
+
+// s2.innerHTML = "tt";
+// s2.style.color = "red";
+
+var arr = [30, "따르릉", true];
+for (var i = 0; i < arr.length; i++) {
+    // s1.innerHTML += "arr[i]" + "<br>";
+document.write(arr[i] );
+// 0 1 2  = i의 길이 3
+// 30 , "따르릉" , true 출력
+}
