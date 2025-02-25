4e08f7725e3da874621faceb7c7fd917621ae381
jjh6965 <jaehun6965@gmail.com>
2025-02-25 12:41:09 +0900

    eventListener_v1

--- 2월/250225/js/eventListener.js
+++ 2월/250225/js/eventListener.js
@@ -2,21 +2,21 @@
 document.getElementById("btn-1").addEventListener("click", dis1);
 //문서의 id 값이 btn-1인 태그에 동작 부위를 연결한다.
 //click을 햇을대 dis1의 함수를 호출한다.
+
 function dis1() {
   document.getElementById("demo1").innerHTML = Date();
 }
 //dis1 함수 호출 문서의 demo1 태그의 HTML은 현재 날짜를 할당.
 
-// document.getElementById("btn-2").addEventListener("click", dis2);
-
-// function dis2() {
+//예제.2~3
+// document.getElementById("btn-2").addEventListener("click", function() {
 //   alert("안녕하세요.");
-// }
+// });
 
-//예제.4
 document.getElementById("btn-2").addEventListener("click", ale1);
 // 위와 같이 분할해도 되지만 이벤트리스너의 함수안에 함수를 넣어도 적용이 된다 = 가독성
 
+//예제.4
 var btn4 = document.getElementById("btn-4");
 btn4.addEventListener("click", ale1);
 btn4.addEventListener("click", ale2);
@@ -56,15 +56,16 @@ window.addEventListener("resize", function () {
 let x7 = 5;
 let y7 = 7;
 var btn7 = document.getElementById("btn-7");
-
+//변수를 할당하고 btn7의 변수에 id값을 할당.
 btn7.addEventListener("click", function () {
   gob(x7, y7);
 });
-
+// 변수의 이벤트 "클릭" 발생했을 때 gob의 함수를 호출한다.
 function gob(a, b) {
   document.getElementById("demo7").innerHTML = a * b;
 }
-
+// gob 함수가 호출 되었을때 demo7 id의 이너html은 axb를 출력한다.
+//출력 할 때 a와b 의 매개 변수에 x7,y7이 대입이 되고 해당 변수는 상단에 값을 할당 해놓았기 때문에 5와 7이 들어간다.
 //예제.8
 document.getElementById("d8-1").addEventListener(
   "click",
@@ -81,6 +82,9 @@ document.getElementById("p8-1").addEventListener(
   },
   false
 );
+// bubbling == false (나 먼저 자식 -> 부모)
+// div > p 를 이벤트를 줬을 때 이벤트가 중복된다.
+// 기본 조건은 div가 바깥쪽이기 때문에 div의 이벤트가 먼저 발생하지만 false줄 경우 p태그 부터 호출된다.
 
 document.getElementById("d8-2").addEventListener(
   "click",
@@ -98,6 +102,9 @@ document.getElementById("p8-2").addEventListener(
   true
 );
 
+//capturing == true (부모 먼저 -> 자식 )
+//true를 할당했을 때 부모 > 자식으로 이벤트가 발생한다.
+
 //예제.9
 document.getElementById("d9-1").addEventListener("mousemove", ran9);
 
@@ -109,3 +116,7 @@ function rem9() {
   document.getElementById("d9-1").removeEventListener("mousemove", ran9);
 }
 
+// div id의 이벤트를 마우스 움직일때 ran9 함수를 호출한다.
+// id가 "demo9"인 p태그에 랜덤 값을 할당하여 마우스가 움직일떄마다 p태그에 랜덤값을 할당한다.
+// div에 리무브이벤트리스너를 주고 움직일때 ran9를 호출
+// html에 onclick 함수를 사용하여 클릭 시 rem9함수를 호춣
