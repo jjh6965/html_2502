956ef56a5dfdef3e8b26735b83db9a760fe16847
jjh6965 <jaehun6965@gmail.com>
2025-02-27 17:21:12 +0900

    0227_css연습

--- 2월/250227/window/js/window.js
+++ 2월/250227/window/js/window.js
@@ -79,7 +79,7 @@ s5.innerHTML += "모니터 해상도 높이: " + sc_h + "px" + "<br>";
 
 /////////////////////////예제.6////////////////////////////////////////
 var phoneNum = "010-2345-1234";
-var result_1 = phoneNum.substring(0, 9);
+var result_1 = phoneNum.substring(9, 0);
 s6.innerHTML = result_1 + "****" + "<br>";
 
 var imgSrc = "images/bnt_out.jpg";
@@ -92,7 +92,8 @@ var menu = ["짜장면", "돈가스", "된장국", "김치찌개", "회덮밥"];
 var menuRan = Math.random();
 
 var menuNum = Math.floor(menuRan * menu.length);
-
+//0~1사이의 난수를 발생하여 배열이 0 1 2 3 4 까지이기에 곱해서 5미만인 수를 floor으로 소수점을 없앤
+//정수를 출력한다 0 1 2 3 4 > 랜덤
 var result_3 = menu[menuNum];
 
 s7.innerHTML = result_3;
